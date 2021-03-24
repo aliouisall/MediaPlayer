8f7754609bfe59bd5b05f937aa1d3b30ce47abf5
Sall Aliou-Ibrahima <aliou-ibrahima.sall@ecl19.ec-lyon.fr>
2021-03-24 08:00:46 +0100

    Extraction of the name of a file

--- Server.js
+++ Server.js
@@ -2,3 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+
+// Extraction du nom d'un fichier. On élémine toutes les \ avant le nom fu fichier. 
+function extpath(dir){
+    var i = dir.lastIndexOf('\\');
+    var ch = ''
+    for(var j=i+1;j<dir.length;j++){
+        ch+=dir[j];
+    }
+        return ch
+    }
